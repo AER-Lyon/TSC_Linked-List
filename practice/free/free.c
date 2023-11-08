d9c5bcc3d58598c2251e4468d4c11046530dae4d
ImperialCrowns <maxime.dziura@epitech.eu>
2023-11-08 21:22:04 +0100

    feat: Display

---
+++ practice/free/free.c
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2023
+** Linked-List
+** File description:
+** creation
+*/
+
+#include "include/my.h"
+
+void free_list(linked_list_t *first)
+{
+    linked_list_t *tmp = first;
+    // TODO: free all the list
+}
---
+++ practice/free/main.c
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2023
+** Linked-List
+** File description:
+** main
+*/
+
+#include "include/my.h"
+
+int main(int argc, char *argv[])
+{
+    linked_list_t *list = create_linked_list(argc, argv);
+    display_list(list);
+    free_list(list);
+    return (0);
+}
