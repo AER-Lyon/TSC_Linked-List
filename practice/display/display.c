1aa2b816975c581a92ed6f537e8efdf51dda78b3
ImperialCrowns <maxime.dziura@epitech.eu>
2023-11-08 21:07:37 +0100

    fix: move folder

---
+++ practice/display/display.c
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2023
+** Linked-List
+** File description:
+** creation
+*/
+
+#include "include/my.h"
+
+void display_list(linked_list_t *first)
+{
+    // TODO: display all of the elements in the list
+}
--- solution/creation.c
+++ solution/creation.c
@@ -10,11 +10,11 @@
 linked_list_t *create_linked_list(int ac, char *av[])
 {
     linked_list_t *begin = malloc(sizeof(linked_list_t));
-    begin->data = av[0];
+    begin->data = av[1];
     begin->next = NULL;
     linked_list_t *current = begin;
 
-    for (int i = 1; i < ac; i++) {
+    for (int i = 2; i < ac; i++) {
         linked_list_t *new_node = malloc(sizeof(linked_list_t));
         new_node->data = av[i];
         new_node->next = NULL;
