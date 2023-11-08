c9c9660b50e3c61088f84618145dc55654f71029
ImperialCrowns <maxime.dziura@epitech.eu>
2023-11-08 22:42:15 +0100

    feat: Add reverse

---
+++ practice/reverse_list/reverse_list.c
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2023
+** Linked-List
+** File description:
+** creation
+*/
+
+#include "include/my.h"
+
+void reverse_list(linked_list_t **begin)
+{
+    linked_list_t *previous = NULL; // TODO: Why NULL?
+    linked_list_t *current = *begin; // TODO: Why *begin?
+    linked_list_t *next = current->next; // TODO: Why current->next?
+
+    // So now we have: current, previous and next. How do we reverse the list?
+}
